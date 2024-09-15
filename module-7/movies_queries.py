af16a0913fe667e8d093f296e81cd65f0deea317
Juliemusic21 <julie.l.sakai@gmail.com>
2024-09-15 02:36:54 -0400

    Update module-8

--- module-7/movies_queries.py
+++ module-7/movies_queries.py
@@ -15,7 +15,7 @@ conn = mysql.connector.connect(**config)
 
 cursor = conn.cursor()
 
-# First query
+# First query to select all the fields for studio table.
 
 cursor.execute("SELECT * FROM studio")
 studios = cursor.fetchall()
@@ -23,28 +23,27 @@ print("-- DISPLAYING Studio Records --")
 for studio in studios:
     print("Studio ID: {}\nStudio Name: {}\n".format(studio[0], studio[1]))
 
-# Second query
+# Second query to select all the fields for genre table.
 cursor.execute("SELECT * FROM genre")
 genres = cursor.fetchall()
 print("\n-- DISPLAYING Genre Records --")
 for genre in genres:
     print("Genre ID: {}\nGenre Name: {}\n".format(genre[0], genre[1]))
 
-# Third query
+# Third query to select the movie names for those movies that have a run time of less than two hours.
 cursor.execute("SELECT film_name, film_runtime FROM film WHERE film_runtime < 120")
 short_movies = cursor.fetchall()
 print("\n-- DISPLAYING Short Film Records --")
 for movie in short_movies:
     print("Film Name: {}\nRuntime: {}".format(movie[0], movie[1]))
 
-# Fourth query
+# Fourth query to get a list of film names, and directors grouped by director. 
 cursor.execute("SELECT film_name, film_director FROM film ORDER BY film_director, film_name")
 films_and_directors = cursor.fetchall()
 print("\n-- DISPLAYING Director RECORDS in Order --")
 for entry in films_and_directors:
     film, director = entry
     print("Film Name: {}\nDirector: {}\n".format(film, director))
-
 
 cursor.close()
 conn.close()
